4c05ce2510256e0b6535dc9a0c7d6cd00dc1b1ef
Nicci Lee Abueg <nicci.abueg@gmail.com>
2020-04-01 20:22:05 -0600

    update

---
+++ python3-dojo/python_stack/myEnvironments/py3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/niccithenomad/sources/DojoAssignments/Python/python3-dojo/python_stack/myEnvironments/py3Env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
